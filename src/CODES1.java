fba62d8cb0110b9226b11146d2d8088f8715fba9
VarunShaw <shaw.varun.06@gmail.com>
2025-12-12 13:28:03 +0530

    intial commit

--- src/CODES1.java
+++ src/CODES1.java
@@ -1,13 +1,12 @@
 public class CODES1 {
     public static void main(String[] args) {
 
-        // Input (Replace with your details)
-        String name = "Your ";
-        String rollNo = "Your Roll No";
-        String section = "Your Section";
-        String branch = "Your Branch";
 
-        // Output
+        String name = "Varun Shaw ";
+        String rollNo = "241551098";
+        String section = "CSE-33";
+        String branch = "AL&ML";
+
         System.out.println("Display Name: " + name);
         System.out.println("Display Roll No: " + rollNo);
         System.out.println("Display Section: " + section);

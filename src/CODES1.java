e3763862cd7570e32f51462978f65e1e347d33df
VarunShaw <shaw.varun.06@gmail.com>
2025-12-12 13:25:48 +0530

    intial commit

---
+++ src/CODES1.java
@@ -0,0 +1,16 @@
+public class CODES1 {
+    public static void main(String[] args) {
+
+        // Input (Replace with your details)
+        String name = "Your ";
+        String rollNo = "Your Roll No";
+        String section = "Your Section";
+        String branch = "Your Branch";
+
+        // Output
+        System.out.println("Display Name: " + name);
+        System.out.println("Display Roll No: " + rollNo);
+        System.out.println("Display Section: " + section);
+        System.out.println("Display Branch: " + branch);
+    }
+}
--- src/labQ1.java
+++
@@ -1,2 +0,0 @@
-public class labQ1 {
-}
